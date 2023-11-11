e24221974cc042f12efc4dcf6e3575ef149cdc5c
abrorm3 <abrormukhammadiev@gmail.com>
2023-11-11 23:46:10 +0500

    fix: paginator with specifi img load

--- darthvaderslaves/src/app/pages/people/card/card.component.ts
+++ darthvaderslaves/src/app/pages/people/card/card.component.ts
@@ -10,19 +10,29 @@ import { PeopleService } from '../people.service';
 export class CardComponent implements OnInit{
   @Input() entity: any;
   @Input() entityType: string ='';
-  @Input() currPage:number=1;
-  adjustedEntityId!: number;
-
+  pageIndex:number = 1;
+  charId:number =0;
   constructor(private router:Router, private route:ActivatedRoute, private peopleService:PeopleService){}
 
   ngOnInit(): void {
-    this.adjustedEntityId = this.entity.id + (this.currPage - 1) * 10;
+
+
+    this.route.queryParams.subscribe(params => {
+      this.pageIndex = params['page'] ? +params['page']: 1;
+      console.log(this.pageIndex + ' Ppp CARD');
+      this.calcIdPage();
+    });
+  }
+  calcIdPage(){
+    this.charId = this.entity.id + (this.pageIndex - 1) * 10;
+    console.log(this.charId);
+
   }
 
   getImagePath(): string {
-      return `assets/${this.entityType}/${this.adjustedEntityId}.jpg`;
+      return `assets/${this.entityType}/${this.charId}.jpg`;
   }
   navigateDetails(){
-    this.router.navigate([`/character/${this.adjustedEntityId}`]);
+    this.router.navigate([`/character/${this.charId}`]);
   }
 }
